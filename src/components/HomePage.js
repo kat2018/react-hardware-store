b5640594fe327b05711904df6d29717e03390bc8
kat2018 <kat.vge@gmail.com>
2018-06-15 11:57:19 -0400

    button changes with ternary

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,7 +18,11 @@ class HomePage extends Component {
 
     //lets type a new function
     handleItermCurrentlyOnSaleChange = (event) => {
-        console.log(event)
+        console.log(event.target.value)
+        //here we will store it so we can change it
+        const value = event.target.value
+    //here we are catching it and changing it in HTML by using the set state that is a function that takes a object
+        this.setState({ itemCurrentlyOnSale: value})
     }
     
     render(){
@@ -28,12 +32,18 @@ class HomePage extends Component {
                 <div>Currently on Sale: {this.state.itemCurrentlyOnSale}!</div>
                 <div>
                     <button onClick={this.toggleEditSaleItem}>
-                        Edit Sale Item
+                    {/* here we created a ternary boolean to change ow the button behaves */}
+                        {this.state.editSaleItem ? "Hide" : "Edit Sale Item"}
+                        
                     </button>
                     
                     {this.state.itemCurrentlyOnSale 
                     ? <div> 
-                        <input type="text" value={this.state.itemCurrentlyOnSale}/> 
+                        <input 
+                        type="text" 
+                        value={this.state.itemCurrentlyOnSale}
+                        onChange={this.handleItermCurrentlyOnSaleChange} 
+                        /> 
                     </div> 
                     : null}
 
